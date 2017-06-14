0678ba90e0561f1c64f309242054c7ae16731e12
aykayPKT <albertkam91@gmail.com>
2017-06-14 16:20:41 -0700

    added, added to cart feedback

--- src/Item.js
+++ src/Item.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import './Item.css';
 
 class Item extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      animation: null
+    }
+  }
+
+  toggleAnimation(){
+    this.props.addToCart(this.props.data);
+    this.setState({
+      animation: "fade 1s 1"
+    })
+    setTimeout(()=>{
+      this.setState({
+        animation: null
+      })
+    }, 1000);
+  }
+
   render() {
     if(this.props.data.product_colors){
       var colors = this.props.data.product_colors.map((val,i) => (
@@ -11,7 +30,8 @@ class Item extends Component {
     return (
       <div className="itemCard">
         <h4 className="name">{this.props.data.name}</h4>
-        <button className="price" onClick={()=> this.props.addToCart(this.props.data)}>${Number(this.props.data.price).toFixed(2)}</button>
+        <button className="price" onClick={this.toggleAnimation.bind(this)}>${Number(this.props.data.price).toFixed(2)}</button>
+        <span style={{"color": "white", "animation": this.state.animation}}>&nbsp;&nbsp;Added to Cart!</span>
         <img className="img" src={this.props.data.image_link} alt={this.props.data.name} />
         <p>Category: {this.props.data.category}</p>
         <p>Colors:</p>
